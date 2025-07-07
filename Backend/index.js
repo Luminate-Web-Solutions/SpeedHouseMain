0f24e229e82f4479f1c7cbe29f551b52c7456818
salehmohiuddin0786 <usamamohiudin0786@gmail.com>
2025-07-07 15:57:01 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3030;
 
 // ✅ CORS Setup — Allow Frontend Origins (Multiple Domains Supported)
 app.use(cors());
