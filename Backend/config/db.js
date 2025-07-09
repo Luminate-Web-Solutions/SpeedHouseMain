14acc9c18c1f1f0173bb43864f072c53d8196618
salehmohiuddin0786 <usamamohiudin0786@gmail.com>
2025-07-09 09:51:13 +0530

    .,

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('SpeedHouse', 'root', '', {
+const sequelize = new Sequelize('SpeedHouse', 'sql_speed_lumina', 'd699678dbae29', {
   host: 'localhost',
   dialect: "mysql"
 });
